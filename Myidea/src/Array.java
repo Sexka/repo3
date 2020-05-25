a53a6be421fbb3d16b1e6316046adca813e2f489
Rika <1316379832@qq.com>
2020-05-25 13:07:09 +0800

    Rika

---
+++ Myidea/src/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("ABC");
+        System.out.println("");
+        System.out.println(23);
+        System.out.println(-500);
+        //整数常量
+        System.out.println(1.2);
+        //浮点数
+        System.out.println('a');
+        //两个单引号之间必须有且只有一个字符没有不行有两个也不行
+        System.out.println(true);
+        System.out.println(false);
+        //布尔值
+    }
+}
---
+++ Myidea/src/Jebin.java
@@ -0,0 +1,14 @@
+public class Jebin {
+    public static void main(String[] args) {
+        jaRy();
+        maRy();
+    }
+    public static void jaRy(){
+        System.out.println("stole");
+    }
+    public static void maRy(){
+        System.out.println("fk");
+        System.out.println("44444");
+        System.out.println(44444);
+    }
+}
